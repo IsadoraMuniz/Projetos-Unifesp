23c7d491181a4914b5d6144d304afd5bca5084c5
Isadora Muniz <37559523+IsadoraMuniz@users.noreply.github.com>
2021-02-04 16:22:14 -0300

    Delete Sistemas Operacionais directory

--- Sistemas Operacionais/Trabalho 2/deadlock-finder-master - everton/terminal.sh
+++
@@ -1,3 +0,0 @@
-cc -Wall -o aplicacao aplicacao.c -lpthread
-cc -Wall -shared -o my_semaphore.so my_semaphore.c -ldl -fPIC
-LD_PRELOAD=./my_semaphore.so ./aplicacao
--- Sistemas Operacionais/Trabalho 2/prog_final/terminal.sh
+++
@@ -1,3 +0,0 @@
-cc -Wall -o nova_aplicacao nova_aplicacao.c -lpthread
-cc -Wall -shared -o novo_my_semaphore.so novo_my_semaphore.c -ldl -fPIC
-LD_PRELOAD=./novo_my_semaphore.so ./nova_aplicacao
